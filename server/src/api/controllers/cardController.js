92885bf57e3a6efd149418f735cf3726324bd1f2
maheshdila <maheshdilshan716@gmail.com>
2023-08-16 19:07:23 +0530

    createCard function established
    createCard function created, yet to be verified

---
+++ server/src/api/controllers/cardController.js
@@ -0,0 +1,39 @@
+const { getUserHash, comparePassword , createUser} = require("../services/userService.js")
+
+const login = async (req, res) => {
+  const username = req.body.username
+  const password = req.body.password
+
+  const hash = await getUserHash(username)
+  if (!hash) {
+    return res.send({ approved: false })
+  }
+
+
+
+  const match = await comparePassword(password, hash)
+  console.log("Match: ", match)
+  if (match) {
+    console.log("Login successful")
+    return res.send({ approved: true })
+  } else {
+    console.log("Login failed")
+    return res.send({ approved: false })
+  }
+}
+
+const register = async (req, res) => {
+  
+  console.log("Registration function")
+  const username = req.body.username
+  const password = req.body.password
+  const userType = req.body.userType
+  
+  const newUser = await createUser(username, password,userType)
+  if(newUser){
+    console.log("Registration successful")
+    return res.send({ approved: true })
+  }
+}
+
+module.exports = { login, register }
--- server/src/api/routes/OrderRoutes.js
+++ server/src/api/routes/OrderRoutes.js
@@ -5,6 +5,7 @@ const { one_way, round_trip, multi_trip } = orderController
 const router = express.Router()
 
 router.get("/")
+//router.post()
 //router.post("/one-way", one_way)
 //router.post("/round-trip", round_trip)
 //router.post("/multi-planet", multi_trip)
