7d5879555bb62099f194cacdc2b69e21a231e6bc
maheshdila <maheshdilshan716@gmail.com>
2023-08-16 19:15:05 +0530

    createCard Function
    modified create card function

--- server/src/api/services/cardService.js
+++ server/src/api/services/cardService.js
@@ -1,36 +1,20 @@
 const { PrismaClient } = require("@prisma/client")
-const bcrypt = require("bcrypt")
+//const bcrypt = require("bcrypt")
 
 const prisma = new PrismaClient()
 
-const getUserHash = async (username) => {
-
-  const user = await prisma.credentials.findFirst({
-    where: {
-      username: username,
-    },
-    select: { password_hash: true },
-  })
-
-  return user ? user.password_hash : null
-}
-
-const comparePassword = async (password, hash) => {
-  return await bcrypt.compare(password, hash)
-}
-const createUser = async (username, password, userType) => {
-  const saltRounds = 10
-  const salt = await bcrypt.genSalt(saltRounds)
-  const hash = await bcrypt.hash(password, salt)
-
-  const user = await prisma.credentials.create({
+const createCard = async (card_number, user_id, expiry_month, expiry_year, cvn ) => {
+  const user = await prisma.stored_cards.create({
     data: {
-      username: username,
-      password_hash: hash,
-      user_type: userType,
+      card_number: card_number,
+      user_id:user_id,
+      expiry_month:expiry_month,
+      expiry_year:expiry_year,
+      cvn:cvn,
     },
   })
 
-  return user
+  return stored_cards
 }
-module.exports = { getUserHash, comparePassword , createUser}
+
+module.exports = { createCard}
