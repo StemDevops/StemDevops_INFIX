ed6d69def2a9b13a431a7cab7181d31b4134ed12
samithkavishke <55777141+samithkavishke@users.noreply.github.com>
2023-08-20 03:17:38 +0530

    Update bookingService.js

--- server/src/api/services/bookingService.js
+++ server/src/api/services/bookingService.js
@@ -2,19 +2,18 @@ const { PrismaClient } = require("@prisma/client")
 
 const prisma = new PrismaClient()
 
-const createUser = async (username, password, userType, universal_id) => {
-  
-
-  const user = await prisma.credentials.create({
-    data: {
-      universal_id: universal_id,
-      username: username,
-      password_hash: hash,
-      user_type: userType,
-      points: 0,
+const getAvailableBookings = async (starting_id, end_id, starting_date) => {
+const shipDetail = await prisma.booking_view.findMany({
+    where: {
+      starting_id: starting_id,
+      end_id: end_id,
+      starting_date: starting_date
     },
+    select: { price: true, name: true, ship_type:true},
   })
 
-  return user
+  return shipDetail
+
 }
-module.exports = { getUserData, comparePassword, createUser }
+
+module.exports = {  getAvailableBookings }
