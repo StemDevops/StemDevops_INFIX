7e3f273506303284024704163c9bdfb9179412c4
samithkavishke <55777141+samithkavishke@users.noreply.github.com>
2023-08-20 03:07:42 +0530

    changed some what

---
+++ server/prisma/views/public/booking_view.sql
@@ -0,0 +1,22 @@
+SELECT
+  t1.starting_id,
+  t1.end_id,
+  t1.booking_id,
+  t1.ship_id,
+  t3.price,
+  t2.name,
+  t2.ship_type
+FROM
+  (
+    (
+      "Available Booking" t1
+      LEFT JOIN "Spaceship" t2 ON ((t1.ship_id = t2.ship_id))
+    )
+    LEFT JOIN "Booking-spaceship" t3 ON (
+      (
+        (t2.ship_type = t3.ship_type)
+        AND (t1.starting_id = t3.starting_id)
+        AND (t1.end_id = t3.end_id)
+      )
+    )
+  );
--- server/src/api/services/bookingService.js
+++ server/src/api/services/bookingService.js
@@ -1,90 +1,20 @@
 const { PrismaClient } = require("@prisma/client")
-const bcrypt = require("bcrypt")
 
 const prisma = new PrismaClient()
 
-
-//to create new spaceship booking entry
-
-const createNewBooking = async (ship_type, starting_id,end_id,price) => {
+const createUser = async (username, password, userType, universal_id) => {
   
 
-  const newBooking = await prisma.booking_spaceship.create({
+  const user = await prisma.credentials.create({
     data: {
-        ship_type: ship_type,
-        starting_id: starting_id,
-        end_id: end_id,
-        price: price
+      universal_id: universal_id,
+      username: username,
+      password_hash: hash,
+      user_type: userType,
+      points: 0,
     },
-    
   })
 
-  return newBooking
+  return user
 }
-//to enter new booking to the available booking table
-const AddBooking = async (booking_id, ship_id,starting_id,end_id,starting_time) => {
-  
-
-    const availableBooking = await prisma.available_Booking.create({
-      data: {
-            booking_id: booking_id,
-            ship_id: ship_id,
-            starting_id: starting_id,
-            end_id: end_id,
-            starting_time: starting_time
-          
-      },
-      
-    })
-  
-    return availableBooking
-  }
-
-  const getAvailableBookings = async (starting_id,end_id,starting_date) => {
-    try {
-      await prisma.$transaction(async (tx) => {
-        const availableBooking = await tx.available_Booking.findMany({
-          where: {
-            starting_id: starting_id,
-            end_id: end_id,
-            starting_date: starting_date
-          },
-          select: {
-            starting_time: true,
-            spaceship:{
-              select:{
-                ship_type: true,
-                name: true, 
-              }
-            },
-            
-            available_seat_count: true,
-            
-          },
-        })
-        const shipTypes = availableBooking.map((booking) => booking.ship_type);
-        const shipPrices = await prisma.$queryRaw
-        `SELECT ship_type, price
-         FROM booking_spaceship
-         WHERE ship_type IN (${shipTypes})`;
-        
-         const bookingsWithPrices = availableBooking.map((booking) => { 
-              const price = shipPrices.find((price) => price.ship_type === booking.ship_type)
-              return {
-                ...booking,
-                price: price.price,
-              }}
-              )
-        return bookingsWithPrices
-        }) 
-    }
-    catch (err) {
-      console.log(err)
-    }
-  }
-  
-
-    
-  
-    
-module.exports = { createNewBooking, AddBooking, getAvailableBookings}
+module.exports = { getUserData, comparePassword, createUser }
